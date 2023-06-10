52e32c1d2533fd2da4f065eec3275d70e90ab845
KleytonFSantos <akatsukipb12@gmail.com>
2023-06-10 01:07:12 -0300

    ajustes no deploy

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,31 +1,29 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, Ref } from "react";
 
 type InputProps = {
-    label: string,
-    name: string,
-    type: string,
-    value?: string
+  label: string,
+  name: string,
+  type: string,
+  value?: string
 }
 
 // eslint-disable-next-line react/display-name
-export const Input: any = forwardRef<any>(({ label, name, type, ...props}: any, ref) => {
-    return (
-      <div>
-        <label
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
-          {label}
-        </label>
-        <div className="mt-2">
-          <input
-            ref={ref}
-            name={name}
-            type={type}
-            autoComplete={name}
-            {...props}
-            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          />
-        </div>
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ label, name, type, ...props }, ref) => {
+  return (
+    <div>
+      <label className="block text-sm font-medium leading-6 text-gray-900">
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          ref={ref}
+          name={name}
+          type={type}
+          autoComplete={name}
+          {...props}
+          className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
       </div>
-    );
-})
+    </div>
+  );
+});
